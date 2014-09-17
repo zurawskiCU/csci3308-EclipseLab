e6bcec67a712316791e2b29f88824ae329d2ecbe
Daniel Zurawski <zurawski@colorado.edu>
2014-09-17 17:21:19 -0600

    2nd commit

--- workspace/hello/src/hello/Hello.java
+++ workspace/hello/src/hello/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); //display the string
-		
-		
+		System.out.println("How are you");
+				
 
 	}
 
